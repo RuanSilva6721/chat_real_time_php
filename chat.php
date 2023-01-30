0339f2f96d6c3020ec570efe5bbb51876eb6840c
RuanSilva2167 <ruansilva6721@gmail.com>
2023-01-30 17:25:08 -0300

    adicionando css

--- chat.php
+++ chat.php
@@ -3,5 +3,5 @@ include_once 'config/conexao.php';
 $sql = $conn->query("SELECT * FROM chat1");
 
 foreach($sql->fetchAll() as $key){
-    echo "<h3>".$key['nome']."</h3> :<p>".$key['mensagem']."</p>";
+    echo "<p id='message' class='alert alert-success' role='alert'>".$key['nome'].": ".$key['mensagem']."</p>";
 } 
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
     <script src="js/script.js"></script>
     <title>ChatRealTime</title>
 </head>
@@ -12,14 +13,14 @@
     <div id="chat">
 
     </div>
-    <form>
+    <form method="post" action="process.php">
   <div class="form-group">
     <label for="nome">Nome</label>
-    <input type="text" class="form-control" id="nome" placeholder="Digite o seu Nome" name="nome">
+    <input name="nome" type="text" class="form-control" id="nome" placeholder="Digite o seu Nome" >
   </div>
   <div class="form-group">
     <label for="mensagem">Mensagem</label>
-    <textarea class="form-control" id="mensagem" rows="3" placeholder="Digite a sua Mensagem" name="mensagem"></textarea>
+    <textarea name="mensagem" class="form-control" id="mensagem" rows="3" placeholder="Digite a sua Mensagem" ></textarea>
   </div>
   <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
